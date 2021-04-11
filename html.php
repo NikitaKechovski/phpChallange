7fdaed1fe2b363112fe6e00dd66cc611c5c2fef3
NikitaKechovski <81694941+NikitaKechovski@users.noreply.github.com>
2021-04-12 01:15:07 +0200

    CSS

--- html.php
+++ html.php
@@ -1,4 +1,42 @@
 <html>
+<style>
+    input[type=text], select {
+        width: 70%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+
+    input[type=submit] {
+        width: 70%;
+        background-color: #89CFF0;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+
+    }
+
+    input[type=submit]:hover {
+        background-color: #89CFF0;
+    }
+
+    div {
+        border-radius: 5px;
+        padding: 20px;
+        margin: auto;
+    }
+    body{
+        background-color: #f2f2f2;
+    }
+</style>
+<body>
+<div>
 <h1>Filter Reviews</h1>
 <form action="Challange.php" method="get">
     <label for="rating">Order by rating</label><br>
@@ -27,4 +65,6 @@
 
     <input type="submit">
 </form>
+</div>
+</body>
 </html>
